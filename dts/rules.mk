486629e7a18c91087ed3545dce4533495a2ad7b0
Kiciuk <kiiciook@gmail.com>
2020-09-10 16:52:53 +0200

    dts:rules.mk: Add tissot.

--- dts/rules.mk
+++ dts/rules.mk
@@ -14,6 +14,7 @@ ifeq ($(PROJECT), msm8953-secondary)
 DTBS += \
 	$(LOCAL_DIR)/sdm450-samsung-r04.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-mido.dtb
+	$(LOCAL_DIR)/sdm625-xiaomi-tissot.dtb
 endif
 ifeq ($(PROJECT), msm8952-secondary)
 DTBS += \
